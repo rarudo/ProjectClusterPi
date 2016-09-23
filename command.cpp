5194ee7f2e23c1956a88a75403e9bfb2cd122fd5
rarudo <rarudonet@gmail.com>
2016-09-23 19:39:37 +0900

    add method "getDNS"

---
+++ command.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 16/09/23.
+//
+
+#include "command.h"
---
+++ command.h
@@ -0,0 +1,14 @@
+//
+// Created by user on 16/09/23.
+//
+
+#ifndef PROJECTPI_COMMAND_H
+#define PROJECTPI_COMMAND_H
+
+
+class command {
+
+};
+
+
+#endif //PROJECTPI_COMMAND_H
