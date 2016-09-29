ab86729934a1d2228741681afb29ef50763f24f3
rarudo <rarudonet@gmail.com>
2016-09-29 13:39:45 +0900

    add traceroute

---
+++ dnsInfo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 16/09/26.
+//
+
+#include "dnsInfo.h"
---
+++ dnsInfo.h
@@ -0,0 +1,14 @@
+//
+// Created by user on 16/09/26.
+//
+
+#ifndef PROJECTPI_DNSINFO_H
+#define PROJECTPI_DNSINFO_H
+
+
+class dnsInfo {
+
+};
+
+
+#endif //PROJECTPI_DNSINFO_H
